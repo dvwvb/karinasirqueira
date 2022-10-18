30340a77927e5f8899345288d7e0473ff6fcfce6
y13564@gmail.com <y13564@gmail.com>
2022-10-18 11:55:50 +0900

    코드정리

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Karina Sirqueira</title>
+    <meta name="description" content="I&#x27;m Karina Sirqueira, a Brazilian designer at Work&amp;Co in Brooklyn." />
+    <meta property="og:title" content="Karina Sirqueira" />
+    <meta property="og:image" content="../images.prismic.io/karina-sirqueira/58ff0832-15aa-41d6-b5e7-f0901a3fb253_Open%2bGraph71fb.jpg?auto=compress,format" />
+    <meta property="og:description" content="I&#x27;m Karina Sirqueira, a Brazilian designer at Work&amp;Co in Brooklyn." />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Karina Sirqueira" />
+    <meta name="twitter:description" content="I&#x27;m Karina Sirqueira, a Brazilian designer at Work&amp;Co in Brooklyn." />
+    <meta name="twitter:image:src" content="../images.prismic.io/karina-sirqueira/58ff0832-15aa-41d6-b5e7-f0901a3fb253_Open%2bGraph71fb.jpg?auto=compress,format" />
+    <script type="module" crossorigin src="assets/main.2570b4e7.js"></script>
+    <link rel="stylesheet" href="assets/main.e7ce3c29.css">
+    <link rel="manifest" href="manifest.webmanifest">
+</head>
+
+<body>
+    <div class="app" data-template="home">
+        <div class="home">
+            <div class="home__wrapper">
+                <div class="home__content">
+                    <div class="home__media">
+                        <div style="--background: #9bce51;">
+                            <a class="home__media__element home__media__element--0" href="projects/clover/index.html" id="clover">
+                                <img alt="Clover" class="home__media__media" data-src="medias/267ddf0e-103e-4319-9ee8-fff0c103613a_clover.svg">
+                            </a>
+                        </div>
+                        <div style="--background: #ffe31b;">
+                            <a class="home__media__element home__media__element--1" href="projects/mailchimp/index.html" id="mailchimp">
+                                <img alt="Mailchimp" class="home__media__media" data-src="medias/94bdb059-d0ce-4e5c-bec2-1b063a161385_mailchimp.svg">
+                            </a>
+                        </div>
+                        <div style="--background: #0a0a0a;">
+                            <a class="home__media__element home__media__element--2" href="projects/mta/index.html" id="mta">
+                                <img alt="MTA" class="home__media__media" data-src="medias/b6844187-24a1-4f5d-8de5-baacb48237f9_mta.svg">
+                            </a>
+                        </div>
+                        <div style="--background: #aeaeae;">
+                            <a class="home__media__element home__media__element--3" href="projects/aldo/index.html" id="aldo">
+                                <img alt="Aldo" class="home__media__media" data-src="medias/1622bf5d-6c44-4d4f-8a25-5646dd02d8ca_aldo.svg">
+                            </a>
+                        </div>
+                        <div style="--background: #cdcccc;">
+                            <a class="home__media__element home__media__element--4" href="projects/wwc/index.html" id="wwc">
+                                <img alt="WWC" class="home__media__media" data-src="medias/d828fa72-c8be-4084-bda0-0caf4bd4ff7f_wwc.svg">
+                            </a>
+                        </div>
+                        <div style="--background: #2157a4;">
+                            <a class="home__media__element home__media__element--5" href="projects/gainbridge/index.html" id="gainbridge">
+                                <img alt="Gainbridge" class="home__media__media" data-src="medias/ee7cbb86-e495-487b-9a9d-aa4d7af1cdc2_gainbridge.svg">
+                            </a>
+                        </div>
+                        <div style="--background: #85cef1;">
+                            <a class="home__media__element home__media__element--6" href="projects/vista/index.html" id="vista">
+                                <img alt="Vista" class="home__media__media" data-src="medias/64c29c4a-1916-4c50-8a86-0cf236add72d_vista.svg">
+                            </a>
+                        </div>
+                    </div>
+
+                    <a class="home__link" href="about/index.html">
+                        Karina Sirqueira
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
